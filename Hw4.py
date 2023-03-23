cad6b9b3342a5cd6d89954b523f8f18e6f428666
wangihsin <61727312+wangishin@users.noreply.github.com>
2023-03-23 23:17:11 +0800

    Add files via upload

---
+++ Hw4.py
@@ -0,0 +1,13 @@
+a = int(input("輸入第一個值"))
+b = int(input("輸入第二個值"))
+c = int(input("輸入第三個值"))
+
+
+if a + b > c and a + c > b and a + c > b:
+    print("可構成三角形")
+elif a + b == c and a + c == b and b + c == a:
+    print("正三角形")
+elif a == b or a == c or b == c:
+    print('等腰三角形')
+else:
+    print("不是三角形")
